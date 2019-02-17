dd721cacdae59e488b8b91ebf362375c5f1f83bd
Sung Won Cho <mikeswcho@gmail.com>
2019-02-17 22:22:06 +1100

    configure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ git push
 echo '### Building'
 hugo
 
-# Assuming aws cli is configured with credentials (`sudo aws configure`)
+# Assuming aws cli is configured with credentials (`aws configure`)
 echo '### Uploading to S3 bucket'
-sudo aws s3 sync --acl=public-read public s3://monomax.sh
+aws s3 sync --acl=public-read public s3://monomax.sh
